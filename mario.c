812a5dce2db02e7117b2af15a9fc502114426f51
andriibessarab <andriibessarab@users.noreply.github.com>
2020-07-03 21:09:06 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -3,36 +3,36 @@
 
 int get_height();
 
-int main(void) {
+int main(void)
+{
 
     //Get height
     int height = get_height();
 
     //Print pyramids by rows
-    for (int row=1; row <= height; row++)
+    for (int row = 1; row <= height; row++)
     {
-
         //Print spaces before the bricks
-        for (int i=1; i <= height-row; i++)
+        for (int i = 1; i <= height - row; i++)
         {
             printf(" ");
         }
 
         //Print left pyramid's bricks
-        for (int i=1; i <= row; i++)
+        for (int i = 1; i <= row; i++)
         {
             printf("#");
         }
 
         //Print spaces between left and right pyramids
         printf("  ");
-        
+
         //Print right pyramid's bricks
-        for (int i=1; i <= row; i++)
+        for (int i = 1; i <= row; i++)
         {
             printf("#");
         }
-        
+
         //Go to a new line
         printf("\n");
     }
@@ -47,7 +47,7 @@ int get_height(void)
         int height = get_int("Height: ");
 
         //Check if height is between 1 and 8
-        if (height >= 1 && height <=8)
+        if (height >= 1 && height <= 8)
         {
             return height;
         }
